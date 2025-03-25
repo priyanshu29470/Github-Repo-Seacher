0799417f2c531527c8211466c35c9b2249fb40b7
PriyanshuSomani <priyanshu.somani@edgescale.in>
2025-03-25 23:04:34 +0530

    Token added while running the script

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Insert token after run.sh command"
+    exit 1
+fi
+
+export GIT_API_TOKEN="$1"
+
 echo "Setting up db"
 cd docker || exit 1
 docker-compose up -d
